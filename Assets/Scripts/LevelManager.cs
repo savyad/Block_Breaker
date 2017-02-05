0eda0dd6da2bb4271017a99a8c708446a06f3191
Savyasachi Dhurve <savya1612@gmail.com>
2017-02-05 21:53:22 +0530

    Beta phase Block_Breaker

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	
+	public void LoadLevel(string name)
+	{
+		Debug.Log("level requested is loading " +name);
+		Application.LoadLevel(name);
+	}
+	public void QuitLevel()
+	{
+		Debug.Log("I wanna Quit ");
+		Application.Quit();
+		
+	}
+	
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+	static MusicPlayer instance = null;
+
+	// Use this for initialization
+	void awake()
+	{
+		Debug.Log("Music player has been awaken " + GetInstanceID());
+	
+	}
+	void Start () {
+		Debug.Log("Music player started " + GetInstanceID());
+		if(instance !=null)
+		{
+			Destroy (gameObject);
+			print("Duplicate music player destroyed");
+		}
+		else{
+			instance = this;
+				GameObject.DontDestroyOnLoad(gameObject);
+		}
+		 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
